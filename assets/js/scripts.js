579dba2dfcc20a9f8029cbdb2cb922ac47761c70
pokmnx <budpok@outlook.com>
2019-10-01 10:53:53 +0300

    animation and query

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 	new WOW().init();
 	$('html').addClass('wow_active');
-	$('.countdown').FlipClock(3600*72);
+
+	const startDate = new Date(2019, 9, 1);
+	const date = new Date();
+	const diff = (date.getTime() - startDate.getTime())/1000;
+	$('.countdown').FlipClock(3600*72 - diff, {
+		countdown: true
+	});
+
+	$(".js-modal-btn").modalVideo({channel:'vimeo'});
+
+	$("#top").click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+	});
 });
